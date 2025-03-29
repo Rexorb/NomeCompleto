0d44081a711a02e2b7c3e3ff6b19d14f8b723afb
Roberto Henrique <roberto_sonicteam@hotmail.com>
2025-03-29 00:58:55 -0300

    Conclusão da atividade

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// Exercício NomeCompleto
+/*Receba o nome e o sobrenome do usuário separadamente. Exiba o nome completo no formato padrão Nome Sobrenome e no formato de catálogo SOBRENOME, Nome.
+
+Exemplo:
+
+Digite seu primeiro nome: Diego
+Digite seu sobrenome: Neri
+
+Nome completo: Diego Neri
+Nome de catálogo: NERI, Diego*/
+Console.WriteLine("Digite seu primeiro nome:");
+String nome = Console.ReadLine();
+Console.WriteLine("Digite seu sobrenome:");
+String sobrenome = Console.ReadLine();
+
+Console.WriteLine($"Seu nome e sobrenome:  {nome} {sobrenome}");
+Console.WriteLine($"Seu nome de catálogo:  {sobrenome.ToUpper()}, {nome}");
+
+
+
+
+
+
